d27e5640718c80fbf636e12ac42d31f1e63b09a4
grapefruitgarv <66042774+grapefruitgarv@users.noreply.github.com>
2023-05-26 12:02:43 -0700

    Initial commit. Add pseudocode.

---
+++ rps.js
@@ -0,0 +1,14 @@
+// variable to store computer's selection of rock, paper, or scissors (computerSelection)
+
+// variable to store user's choice of rock, paper, or scissors (playerSelection)
+// computer selects from the options of rock, paper, or scissors and stores it computerSelection
+// if...else if statements comparing the two variables
+// if comp compSel is rock and userCho is scissors you lose rock beats scissors keeping it case insensitive
+// if comp compSel is rock and userCho is paper you win paper beats rock keeping it case insensitive
+// if comp compSel is rock and userCho is rock you tie keeping it case insensitive
+// if comp compSel is paper and userCho is paper you tie keeping it case insensitive
+// if comp compSel is paper and userCho is rock you lose paper beats rock keeping it case insensitive
+// if comp compSel is paper and userCho is scissors you win scissors beats paper keeping it case insensitive
+// if comp compSel is scissors and userCho is paper you lose scissors beats paper keeping it case insensitive
+// if comp compSel is scissors and userCho is rock you win rock beats scissors keeping it case insensitive
+// if comp compSel is scissors and userCho is scissors you tie keeping it case insensitive
