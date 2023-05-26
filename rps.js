0094f89cecae28847e8136eb4584769d09a6bb9c
grapefruitgarv <66042774+grapefruitgarv@users.noreply.github.com>
2023-05-26 12:29:25 -0700

    Add .gitignore. Feat add func to get random selection
    Randomly assign rock, paper, or scissors to the
    computerSelection variable through the function getComputerChoice().

--- rps.js
+++ rps.js
@@ -1,14 +1,29 @@
 // variable to store computer's selection of rock, paper, or scissors (computerSelection)
-
+const computerSelection = getComputerChoice();
 // variable to store user's choice of rock, paper, or scissors (playerSelection)
+const playerSelection = "";
 // computer selects from the options of rock, paper, or scissors and stores it computerSelection
+function getComputerChoice() {
+  randNum = Math.floor(Math.random() * 3 + 1);
+  if (randNum == 1) {
+    return "rock";
+  } else if (randNum == 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+console.log(computerSelection);
 // if...else if statements comparing the two variables
-// if comp compSel is rock and userCho is scissors you lose rock beats scissors keeping it case insensitive
-// if comp compSel is rock and userCho is paper you win paper beats rock keeping it case insensitive
-// if comp compSel is rock and userCho is rock you tie keeping it case insensitive
-// if comp compSel is paper and userCho is paper you tie keeping it case insensitive
-// if comp compSel is paper and userCho is rock you lose paper beats rock keeping it case insensitive
-// if comp compSel is paper and userCho is scissors you win scissors beats paper keeping it case insensitive
-// if comp compSel is scissors and userCho is paper you lose scissors beats paper keeping it case insensitive
-// if comp compSel is scissors and userCho is rock you win rock beats scissors keeping it case insensitive
-// if comp compSel is scissors and userCho is scissors you tie keeping it case insensitive
+function playRound(playerSelection, computerSelection) {
+  // if comp computerSelection is rock and playerSelection is scissors you lose rock beats scissors keeping it case insensitive
+  // if comp computerSelection is rock and playerSelection is paper you win paper beats rock keeping it case insensitive
+  // if comp computerSelection is rock and playerSelection is rock you tie keeping it case insensitive
+  // if comp computerSelection is paper and playerSelection is paper you tie keeping it case insensitive
+  // if comp computerSelection is paper and playerSelection is rock you lose paper beats rock keeping it case insensitive
+  // if comp computerSelection is paper and playerSelection is scissors you win scissors beats paper keeping it case insensitive
+  // if comp computerSelection is scissors and playerSelection is paper you lose scissors beats paper keeping it case insensitive
+  // if comp computerSelection is scissors and playerSelection is rock you win rock beats scissors keeping it case insensitive
+  // if comp computerSelection is scissors and playerSelection is scissors you tie keeping it case insensitive
+  // if comp computerSelection is scissors and playerSelection is paper computer wins scissors beats paper keeping it case insensitive
+}
